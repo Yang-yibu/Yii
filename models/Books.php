b350778af12c02d59fb3ed79e9f19df8ac01b893
Yang-yibu <18335834527@163.com>
2019-01-20 21:55:02 +0800

    更新时自动修改时间

--- models/Books.php
+++ models/Books.php
@@ -5,8 +5,10 @@
  * Date: 2019/1/18
  * Time: 0:49
  */
+
 namespace app\models;
 
+use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 use Yii;
 
@@ -17,6 +19,21 @@ class Books extends ActiveRecord
         //默认连接 book 表名；
         // 此处修改自定义表名
         return 't_books';
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    // 创建时更新 到 create_time 和 update_time 字段
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['create_time', 'update_time'],
+                    ActiveRecord::EVENT_AFTER_UPDATE => ['update_time']
+                ],
+                'value' => date('Y-m-d H:i:s')
+            ]
+        ];
     }
 
     public function rules()
@@ -34,10 +51,10 @@ class Books extends ActiveRecord
     public function attributeLabels()
     {
         return [
-            'name'   => Yii::t('app', 'bookName'),
+            'name' => Yii::t('app', 'bookName'),
             'author' => Yii::t('app', 'bookAuthor'),
-            'price'  => Yii::t('app', 'price'),
-            'desc'   => Yii::t('app', 'bookDesc'),
+            'price' => Yii::t('app', 'price'),
+            'desc' => Yii::t('app', 'bookDesc'),
         ];
     }
 }
